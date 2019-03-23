90d84d466b269adbe275bba2d1e87f37310ddc4c
Tsao, Cheng-Lin <cheng-lin.tsao@honeywell.com>
2019-03-23 19:52:32 -0400

    Create a SpringBoot project.

---
+++ src/main/java/com/example/bulkinsert/BulkinsertApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulkinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkinsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkinsertApplication.class, args);
+	}
+
+}
